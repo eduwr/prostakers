eaf4fb702bd5095f4a67465e2a119982342cc2d1
eduwr <eduwric@gmail.com>
2022-07-10 14:37:37 -0300

    allow users to stake ETH to the contract.

--- contracts/ProStakers.sol
+++ contracts/ProStakers.sol
@@ -2,6 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract ProStakers {
+    address[] public stakers;
+    mapping(address => bool) public isStaker;
+    mapping(address => uint) public stakedAmount;
 
-    function stake(uint256 _amount) public {}
+    function deposit() public payable {
+        require(msg.value >= 0.01 ether, "Minimum amount is 0.01");
+
+        stakers.push(msg.sender);
+        isStaker[msg.sender] = true;
+        stakedAmount[msg.sender] = msg.value;
+    }
 }
--- test/ProStakers.ts
+++ test/ProStakers.ts
@@ -24,10 +24,17 @@ describe("ProStakers", () => {
     });
   });
 
-  describe("Stake", () => {
+  describe("Deposit", () => {
     it("Should not to throw when staker call stake function", async () => {
       const amount = ethers.utils.parseEther("0.1");
-      expect(await proStakers.connect(staker1).stake(amount)).to.not.throw;
+      expect(await proStakers.connect(staker1).deposit({ value: amount })).to
+        .not.throw;
+    });
+
+    it("Should throw if value is less than 0.01 ETH", async () => {
+      const amount = ethers.utils.parseEther("0.001");
+      const trX = proStakers.connect(staker1).deposit({ value: amount });
+      expect(trX).eventually.to.rejectedWith(Error, "Minimum amount is   0.01");
     });
   });
 });
